8875f460de47569d6977467cf1871659f8ecabe2
subikstha <subsatch@gmail.com>
2026-09-03 10:56:35 +0545

    docs: added some documentation on redux store RootState and AppDispatch types

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "../features/tasks/taskSlice"
-import userReducer from "../features/tasks/userSlice"
-import uiReducer from "../features/tasks/uiSlice"
+import userReducer from "../features/user/userSlice"
+import uiReducer from "../features/ui/uiSlice"
 
 export const store = configureStore({
     reducer: {
@@ -19,8 +19,39 @@ ReturnType<typeof store.getState> automatically gets its type
 therefore type RootState = {
     allTasks: TasksState
 } we donot have to manually maintain that type
+typeof store.getState means "What is the type of getState function?"
+ReturnType<Typeof store.getState> means "What does getState() return?"
+Therefore RootState becomes
+RootState = {
+allTasks: ...
+users: ...
+ui: ...
+}
+
+const user = useSelector(
+    (state: RootState) => state.user
+);
+RootState is useful because it helps TS understand state.users, state.ui, state.allTasks etc
+and also helps in catching mistakes like state.somethingThatDoesNotExist will generate an error
 
 typeof store.dispatch gets the type of Redux dispatch, we will use that to create a typed hook
 
 
+
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+/*
+export type AppDispatch = typeof store.dispatch;
+this will get the type of the store's dispatch function with the help of which you can create typed hooks
+export const useAppDispatch = () => useDispatch<AppDispatch>() and
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+And then the components can simply do
+const dispatch = useAppDispatch();
+
+const user = useAppSelector(
+    state => state.user
+);
 */
---
+++ src/components/Modal.ts
@@ -0,0 +1,6 @@
+import {useEffect, useRef} from 'react'
+import { createPortal } from 'react-dom'
+
+const Modal = () => {}
+
+export default Modal
